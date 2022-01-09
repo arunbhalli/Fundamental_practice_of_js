ca420c3490f87f563cddd8c3acabd94cb4e515d0
arunbhalli <tomarbhalli@gmail.com>
2022-01-09 15:03:08 +0100

    adds examples of different operators

---
+++ basicOperator.js
@@ -0,0 +1,129 @@
+
+//=  assignment operator 
+let x = 2
+console.log(x)
+
+//+  addition operator
+add=2 + 3 
+console.log(add)
+//-  subtraction operator
+minus=2 - 3 
+console.log(minus)
+
+
+//*  multiplicative operator
+multiply=2 * 3 
+console.log(multiply)
+
+
+//  division operator
+division=3 / 2 
+console.log(division)
+
+//%  modulus operator
+module=3 % 2 
+console.log(module)
+
+//++  increment operator
+var z = 9
+z++
+console.log(z)
+// x now equals 10 (z = z + 1 in literal notation)
+
+
+//--  decrement operator
+var t = 3
+t--
+console.log(t)
+// rest are the examples below
+
+// x now equals 2 (t = t - 1 in literal notation)
+// //+=  additive reassignment operator
+// var x = 2
+// x += 3
+// // x now equals 5 (x = x + 3 in literal notation)
+
+
+// //-=  subtractive reassignment operator
+// var x = 3
+// x -= 2
+// // x now equals 1 (x = x - 2 in literal notation)
+
+
+// //*=  multiplicative reassignment operator
+// var x = 3
+// x *= 2
+// // x now equals 6 (x = x * 2 in literal notation)
+
+
+// ///=  division reassignment operator
+// x = 3
+// x /= 2
+// // x now equals 1.5 (x = x / 2 in literal notation)
+
+
+// //%=  modulus reassignment operator
+// x = 3
+// x %= 2
+// // x now equals 1 (x = x % 2 in literal notation)
+
+
+// //==  logical comparison operator
+// 2 == 2 // Returns true
+// 2 == "2" // Returns true
+// 2 == 3 // Returns false
+
+
+// //===  logical strict comparison operator
+// 2 === 2 // Returns true
+// 2 === "2" // Returns false
+// 2 === 3 // Returns false
+
+
+// //!=  logical not equal comparison operator
+// 2 != 3 // Returns true
+// 2 != 2 // Returns false
+// 2 != "2" // Returns false
+
+
+// //!==  logical not equal strict comparison operator
+// 2 !== 3 // Returns true
+// 2 !== 2 // Returns false
+// 2 !== "2" // Returns true
+
+
+// //<  logical less than comparison operator
+// 2 < 3 // Returns true
+// 3 < 2 // Returns false
+
+
+// //>  logical greater than comparison operator
+// 3 > 2 // Returns true
+// 2 > 3 // Returns false
+// 3 > 3 // Returns false
+
+
+// //<=  logical less than or equal to comparison operator
+// 2 <= 3 // Returns true
+// 3 <= 3 // Returns true
+// 3 <= 2 // Returns false
+
+
+// //>= logical less than or equal to comparison operator
+// 3 >= 2 // Returns true
+// 3 >= 3 // Returns true
+// 2 >= 3 // Returns false
+
+
+// //|| logical or operator
+// // x = 3
+// // (x == 3 || x == 2) // Returns true
+// // (x == 2 || x == 1) // Returns false
+
+
+// // //&& logical and operator
+// // x = 2
+// // y = 3
+// // (x == 2 && y == 3) // Returns true
+// // (x == 2 && y == 4) // Returns false
+// console.log(x)
