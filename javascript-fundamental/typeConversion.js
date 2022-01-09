d4be2beb603275cc021afbdd2ef66a7b7fcc99e0
arunbhalli <tomarbhalli@gmail.com>
2022-01-09 20:05:10 +0100

    adds few examples of conversions

---
+++ javascript-fundamental/typeConversion.js
@@ -0,0 +1,23 @@
+// type conversions ans Coercion
+
+// minus converts string into number and + converts no into string....
+let n = '1'+1
+n =n-1
+console.log(n)
+
+let num = 1+2+3+4+"5"
+console.log(num)
+
+
+
+// minusNumber("3.14")    // returns 3.14
+Number(" ")       // returns 0
+console.log(Number("") )       // returns 0
+console.log(Number("99 88") )  // returns NaN
+console.log(String(100 + 23))
+console.log((123).toString())
+console.log(String(Date()) )
+d = new Date();
+console.log(d.getTime() )
+console.log(String(false) )
+console.log(String(true)  )
