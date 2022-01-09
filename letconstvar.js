efdd3f2ce622636aaae1d09b3a0f4579ddc9fed8
arunbhalli <tomarbhalli@gmail.com>
2022-01-09 14:28:10 +0100

    adding examples of var variable

--- letconstvar.js
+++ letconstvar.js
@@ -10,6 +10,33 @@ Name="rahul"
 console.log(Name,typeof Name)//TypeError: Assignment to constant variable.
 
 
+var variable1 // declared using var
+const variable2 // declared using const
+
+myFunction1();
+
+function myFunction1() {
+  variable1 = "hello!";
+  console.log(variable1);
+  // "hello!"
+}
+
+myFunction2();
+
+function myFunction2() {
+  variable2 = "hello!";
+  // error: variable2 is a constant and can not be redifined
+}
+
+myFunction3(true);
+
+myFunction3(codition) {
+  if(condition) {
+    let variable3 = "helo!" // declared using const
+  }
+  variable3 = "hello!";
+  // error: variable3 is out of scope
+}
 
 
 /*var:
@@ -18,7 +45,7 @@ console.log(Name,typeof Name)//TypeError: Assignment to constant variable.
  -can be changed again and again anywhere
 
 
- 
+
 let:
  -can be declared outside any function to be used inside any function
  -if declared inside any function or any other {} which are of only if or if-else or switch etc. and can't be used anywhere inside the function and can be only used inside statement
