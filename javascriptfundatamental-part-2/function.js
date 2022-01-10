e13d0f994ee15a2ba52d24b087223d41546e9e06
arunbhalli <tomarbhalli@gmail.com>
2022-01-10 19:50:10 +0100

    adds function examples for practice

---
+++ javascriptfundatamental-part-2/function.js
@@ -0,0 +1,18 @@
+// calling a function that// Running a function//Invoking a function
+function logger() {
+  console.log('my name is Arun');
+}
+logger();
+logger();
+logger();
+
+function fruitProcessor(apple, oranges) {
+  console.log(apple, oranges);
+  let juice = `juice withh ${apple} apples and ${oranges} oranges`;
+  return juice;
+}
+const mixJuice = fruitProcessor(5, 3);
+console.log(mixJuice);
+const mixJuice2 = fruitProcessor(3, 2);
+console.log(mixJuice2);
+constructor(fruitProcessor(5, 3));
