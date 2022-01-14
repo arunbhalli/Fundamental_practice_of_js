3e0a94dd691b838e8c37302cc028d48185a0f691
arunbhalli <tomarbhalli@gmail.com>
2022-01-14 20:27:17 +0100

    adds examples for finding best friend from friends list

--- javascriptfundatamental-part-2/objects.js
+++ javascriptfundatamental-part-2/objects.js
@@ -11,14 +11,29 @@ const jonasObject = {
   last_name: 'Singh',
   birthYear: 2022 - 1997,
   occupation: 'Teacher',
-  friends: ['Rahul', 'Rohan', 'Mohit'],
+  friends: ['Rahul', 'Rohan', 'Mohit', 'Gaurav', 'Ajay'],
 };
-console.log(jonasObject.last_name)
+console.log(jonasObject.last_name);
 
-const jonas = prompt('Input your inquiry')
+//const jonas = prompt('Input your inquiry')
 
-if (jonasObject[jonas]) {
+/*if (jonasObject[jonas]) {
   console.log(jonasObject[jonas])
 }else {
   console.log('this data in not in our system data')
+}*/
+
+const bestfriend = 'Rohan';
+
+let n = jonasObject.friends.length;
+n = n - 1;
+if (jonasObject.friends[0] == bestfriend) {
+  console.log(`best friend is ${bestfriend}`);
+} else if (
+  jonasObject.friends[1] == bestfriend ||
+  jonasObject.friends[n] == bestfriend
+) {
+  console.log(` ${bestfriend} is a good friend`);
+} else {
+  console.log(`${bestfriend} is not in the friends list `);
 }
