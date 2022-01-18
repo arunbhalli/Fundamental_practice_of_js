e396ee9e6ec495e2df1c9ac227b24d937a7445e8
arunbhalli <tomarbhalli@gmail.com>
2022-01-18 17:55:17 +0100

    adds if else conditions to check whos bmi is greater

--- javascriptfundatamental-part-2/codingChallege/coddingChallenge3.js
+++ javascriptfundatamental-part-2/codingChallege/coddingChallenge3.js
@@ -16,25 +16,32 @@ const johnData = {
 };
 console.log(johnData.johnBmi());
 const markData = {
-  first_name: 'John',
+  first_name: 'Mark',
   last_name: 'Singh',
   birthYear: 1997,
-  mass: 92,
-  height: 1.95,
+  mass: 60,
+  height: 1.2,
   occupation: 'Teacher',
   friends: ['Rahul', 'Rohan', 'Mohit', 'Gaurav', 'Ajay'],
   hasDriversLicense: true,
   markBmi: function () {
     bmiOfMark = this.mass / this.height ** 2;
-    console.log(bmiOfMark);
     return bmiOfMark;
   },
 };
-if (johnData.johnBmi > markData.markBmi) {
-  return console.log(`${this.name}${this.last_name} 
-  He works as a ${this.occupation}.
-  He was born in ${this.birthYear}.
-  He is ${this.calcAge()} Years old.`);
-}
-
-console.log(markData.markBmi());
+if (johnData.johnBmi() > markData.markBmi()) {
+  return console.log(`${johnData.first_name} ${johnData.last_name}
+  He works as a ${johnData.occupation}.
+  He was born in ${johnData.birthYear}.
+  He has ${johnData.johnBmi()} BMI greater than ${
+    markData.first_name
+  }'s ${markData.markBmi()} BMI
+  `);
+} else
+  console.log(`${markData.first_name} ${markData.last_name}
+He works as a ${markData.occupation}.
+He was born in ${markData.birthYear}.
+He has ${markData.markBmi()} BMI greater than ${
+    johnData.first_name
+  }'s ${johnData.johnBmi()} BMI
+`);
